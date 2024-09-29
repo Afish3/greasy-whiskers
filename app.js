b63e6aa6ad55b7c0f6e2aa0c5b8704a3d19fd62f
Aedan Fish <aedanakfish3@gmail.com>
2024-09-29 19:42:53 +0100

    bio section

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fetch = require('node-fetch');
 const ExpressError = require('./expressError');
 
 const app = express();
@@ -7,6 +8,27 @@ app.use('/',express.static(__dirname));
 
 app.get("/", (req, res) => {
     return res.sendFile(__dirname + "/index.html");
+});
+
+app.get('/proxy-image', async (req, res) => {
+  const imageUrl = req.query.url;
+  
+  if (!imageUrl) {
+    return res.status(400).send('No image URL provided');
+  }
+
+  try {
+    const response = await fetch(imageUrl);
+    if (!response.ok) throw new Error('Network response was not ok');
+    
+    const contentType = response.headers.get('content-type');
+    res.setHeader('Content-Type', contentType);
+    
+    response.body.pipe(res);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).send('Error fetching image');
+  }
 });
 
 // 404 handler
--- gallery.js
+++ gallery.js
@@ -1,25 +1,75 @@
-$('.portfolio-menu ul li').click(function(){
-    $('.portfolio-menu ul li').removeClass('active');
-    $(this).addClass('active');
+// $('.portfolio-menu ul li').click(function(){
+//     $('.portfolio-menu ul li').removeClass('active');
+//     $(this).addClass('active');
     
-    var selector = $(this).attr('data-filter');
-    $('.portfolio-item').isotope({
-        filter:selector
-    });
-    return  false;
-});
+//     var selector = $(this).attr('data-filter');
+//     $('.portfolio-item').isotope({
+//         filter:selector
+//     });
+//     return  false;
+// });
+// $(document).ready(function() {
+//     var popup_btn = $('.popup-btn');
+//     popup_btn.magnificPopup({
+//         type : 'image',
+//         gallery : {
+//             enabled : true
+//         }
+//     });
+
+//     var selector = $('.vids').attr('data-filter');
+//     $('.portfolio-item').isotope({
+//         filter:selector
+//     });
+//     return  false;
+// });
+
+
 $(document).ready(function() {
+    // Function to load Google Drive images
+    function loadGoogleDriveImages() {
+        $('.portfolio-item img').each(function() {
+          var $img = $(this);
+          var src = $img.attr('src');
+          if (src.includes('drive.google.com')) {
+            var proxyUrl = '/proxy-image?url=' + encodeURIComponent(src);
+            $img.attr('src', proxyUrl);
+          }
+        });
+      }
+
+    // Existing click handler for portfolio menu items
+    $('.portfolio-menu ul li').click(function(){
+        $('.portfolio-menu ul li').removeClass('active');
+        $(this).addClass('active');
+        
+        var selector = $(this).attr('data-filter');
+        $('.portfolio-item').isotope({
+            filter: selector
+        });
+        return false;
+    });
+
+    // Magnific Popup setup
     var popup_btn = $('.popup-btn');
     popup_btn.magnificPopup({
-        type : 'image',
-        gallery : {
-            enabled : true
+        type: 'image',
+        gallery: {
+            enabled: true
         }
     });
 
+    // Initial filter setup
     var selector = $('.vids').attr('data-filter');
     $('.portfolio-item').isotope({
-        filter:selector
+        filter: selector
     });
-    return  false;
+
+    // Load Google Drive images
+    loadGoogleDriveImages();
+
+    // Re-layout Isotope after images are loaded
+    $('.portfolio-item').imagesLoaded().progress(function() {
+        $('.portfolio-item').isotope('layout');
+    });
 });
