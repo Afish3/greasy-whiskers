0c73887d25c9fb202edb6562ef7932aa4a08fa79
Aedan Fish <aedanakfish3@gmail.com>
2024-09-29 19:45:27 +0100

    bio section

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const fetch = require('node-fetch');
 const ExpressError = require('./expressError');
 
 const app = express();
@@ -8,27 +7,6 @@ app.use('/',express.static(__dirname));
 
 app.get("/", (req, res) => {
     return res.sendFile(__dirname + "/index.html");
-});
-
-app.get('/proxy-image', async (req, res) => {
-  const imageUrl = req.query.url;
-  
-  if (!imageUrl) {
-    return res.status(400).send('No image URL provided');
-  }
-
-  try {
-    const response = await fetch(imageUrl);
-    if (!response.ok) throw new Error('Network response was not ok');
-    
-    const contentType = response.headers.get('content-type');
-    res.setHeader('Content-Type', contentType);
-    
-    response.body.pipe(res);
-  } catch (error) {
-    console.error('Error:', error);
-    res.status(500).send('Error fetching image');
-  }
 });
 
 // 404 handler
